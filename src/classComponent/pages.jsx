75419cd36c749f01e5ea1b2304ef54401d3f98e3
amypo-reactapp <reactapp@amypo.in>
2024-02-20 03:28:14 +0000

    User

---
+++ src/classComponent/pages.jsx
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+
+class ClassCounter extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            count:0,
+        };
+    }
+}
